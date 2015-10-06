92fbf25e35e15e7ceec141e54d5de4354a80e331
BJK <bernie.kravitz@gmail.com>
2015-10-06 15:19:23 -0400

    finished the informRegisteredStore test helper

--- test/index.js
+++ test/index.js
@@ -1,2 +1,6 @@
 var libContext = require.context("./lib/", true, /Spec.js$/);
 libContext.keys().forEach(libContext);
+
+import FUHelpers from './support/SinonAddons'
+
+chai.use(FUHelpers)
--- test/lib/specHelperSpec.js
+++ test/lib/specHelperSpec.js
@@ -7,7 +7,8 @@ import KeyMirror from 'keymirror'
 
 var TestAction = assign(ActionPrototype, {
   Types: KeyMirror({
-    GOT_THING: null
+    GOT_THING: null,
+    NOTHING: null
   }),
   fetchThing: function() {
     //actually fire API 
@@ -15,58 +16,24 @@ var TestAction = assign(ActionPrototype, {
       actionType: TestAction.Types.GOT_THING,
       data: {foo: 'bar'}
     })
+
+  },
+  fetchWithWrongDispatch: function() {
+    Dispatcher.dispatch({
+      actionType: TestAction.Types.NOTHING,
+      data: {foo: 'bar'}
+    })
   }
 })
 
 var TestStore = StorePrototype(TestAction.Types.GOT_THING)
 
-var fuHelpers = function (chai, utils) {
-  var Assertion = chai.Assertion;
-
-  Assertion.addMethod('informRegisteredStore', function(store) {
-    var action = this._obj;
-    var mockPayload = {foo: 'bar'}
-    var callbackAlreadyFired = false
-    //possibly stub jsonFetcher
-
-    var callback = function() {
-      callbackAlreadyFired = true
-      store.removeChangeListener(callback)
-      this.assert(
-        store.getData().foo === mockPayload.foo, //make sure we've heard from 
-        "expected #{this} to dispatch a payload to #{store}",
-        "expected #{this} not to dispatch a payload to #{storeName}"
-      )
-    }.bind(this)
-
-    //register listener with the store
-    store.addChangeListener(callback)
-
-    //fire the action
-    action()
-
-    //possibly some kind of timeout loop to fail if the timeout didn't happen
-
-    //store the output
-
-    //no matter what unregister
-
-    //fire the assertion synchronously
-
-  });
-};
-
-chai.use(fuHelpers)
-
-
 describe("Test Action", function() {
-
-  beforeEach(function() {
-    //featureFlags.init(flags);
-    //some stuff
+  it("dispatches to test store", function(done) {
+    expect(TestAction.fetchThing).to.eventually(done).informRegisteredStore(TestStore)
   })
 
-  it("dispatches to test store", function() {
-    expect(TestAction.fetchThing).to.informRegisteredStore(TestStore)
+  it("wrong action will not notify the store", function(done) {
+    expect(TestAction.fetchWithWrongDispatch).to.not.eventually(done).informRegisteredStore(TestStore)
   })
 })
