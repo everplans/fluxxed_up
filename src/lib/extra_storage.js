1f930e0a6a7e29d4a55ef38095570f161dd7bd11
Bart Riordan <bartriordan@gmail.com>
2015-11-21 13:53:32 -0500

    Clean up extra_storage

--- src/lib/extra_storage.js
+++ src/lib/extra_storage.js
@@ -1,36 +1,30 @@
 
-var storage = {};
+var storage = {}
 
 var extra_storage = {
   getItem: function(key) {
     try {
-      window.sessionStorage.setItem('__extra_storage-flag__', 'test');
-      return window.sessionStorage.getItem(key);
-    }
-    catch(error) {
-      return storage[key];
+      window.sessionStorage.setItem('__extra_storage-flag__', 'test')
+      return window.sessionStorage.getItem(key)
+    } catch (error) {
+      return storage[key]
     }
   },
-
   setItem: function(key, value) {
     try {
-      window.sessionStorage.setItem(key, value);
-    }
-    catch(error) {
-      storage[key] = value;
+      window.sessionStorage.setItem(key, value)
+    } catch (error) {
+      storage[key] = value
     }
   },
-
   removeItem: function(key) {
     try {
-      return window.sessionStorage.removeItem(key);
-    }
-    catch(error) {
-      storage[key] = undefined;
-      return undefined;
+      return window.sessionStorage.removeItem(key)
+    } catch (error) {
+      storage[key] = undefined
+      return undefined
     }
   }
+}
 
-};
-
-module.exports = extra_storage;
+module.exports = extra_storage
