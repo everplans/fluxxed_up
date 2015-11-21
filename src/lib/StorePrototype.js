954f6088c695308baee1d9425627d6ec667ee82e
Bart Riordan <bartriordan@gmail.com>
2015-11-21 14:46:17 -0500

    Fix huge indentation issue

--- src/lib/StorePrototype.js
+++ src/lib/StorePrototype.js
@@ -14,84 +14,83 @@ import AppDispatcher from '../lib/ep-dispatcher'
 // callbacks, add your own methods, etc...
 
 var factory = function(registerAction, extendType, registerMessageAction) {
-  
   var StorePrototype = assign(EventEmitter.prototype, {
-  _errors: [],
-  _msg: null,
-  _data: {},
-  emitChange: function() {
-    this.emit('CHANGE');
-  },
-  addChangeListener: function(callback) {
-    this.on('CHANGE', callback);
-  },
-  removeChangeListener: function (callback) {
-    this.removeListener('CHANGE', callback);
-  },
-  getMsg: function() {
-    return this._msg;
-  },
-  setMsg: function(msg) {
-    this._msg = msg
-  },
-  clearFlash: function() {
-    this._msg = null;
-    this.emitChange();
-  },
-  getErrors: function() {
-    return this._errors;
-  },
-  pushError: function(error) {
-    this._errors.push(error);
-  },
-  setErrors: function(errors) {
-    this._errors = errors;
-  },
-  clearErrors: function() {
-    this._errors = [];
-  },
-  hasErrors: function() {
-    return (this._errors.length > 0)
-  },
-  setData: function(data) {
-    this._data = data;
-  },
-  getData: function() {
-    return this._data;
-  },
-  getState: function() {
-    return {data: this._data, errors: this._errors, message: this._msg}
-  },
-  clearState: function(){
-    this.setData({});
-  },
-  receiveData: function(data) {
-    if (data.errors)
-      this.setErrors(data.errors);
-    else {
-      this.setData(data);
+    _errors: [],
+    _msg: null,
+    _data: {},
+    emitChange: function() {
+      this.emit('CHANGE');
+    },
+    addChangeListener: function(callback) {
+      this.on('CHANGE', callback);
+    },
+    removeChangeListener: function (callback) {
+      this.removeListener('CHANGE', callback);
+    },
+    getMsg: function() {
+      return this._msg;
+    },
+    setMsg: function(msg) {
+      this._msg = msg
+    },
+    clearFlash: function() {
+      this._msg = null;
+      this.emitChange();
+    },
+    getErrors: function() {
+      return this._errors;
+    },
+    pushError: function(error) {
+      this._errors.push(error);
+    },
+    setErrors: function(errors) {
+      this._errors = errors;
+    },
+    clearErrors: function() {
+      this._errors = [];
+    },
+    hasErrors: function() {
+      return (this._errors.length > 0)
+    },
+    setData: function(data) {
+      this._data = data;
+    },
+    getData: function() {
+      return this._data;
+    },
+    getState: function() {
+      return {data: this._data, errors: this._errors, message: this._msg}
+    },
+    clearState: function(){
+      this.setData({});
+    },
+    receiveData: function(data) {
+      if (data.errors)
+        this.setErrors(data.errors);
+      else {
+        this.setData(data);
+        this.clearErrors();
+      }
+      this.emitChange();
+    },
+    receiveMsg: function(msg) {
+      this.setMsg(msg);
       this.clearErrors();
+      this.emitChange();
+    },
+    registerRecieveCallback: function(actionType) {
+      return AppDispatcher.register(function(action) {
+        if (action.actionType == actionType)
+          this.receiveData(action.data);
+      }.bind(this))
+    },
+    registerMessageCallback: function(actionType) {
+      return AppDispatcher.register(function(action) {
+        if (action.actionType == actionType)
+          this.receiveMsg(action.data);
+      }.bind(this));
     }
-    this.emitChange();
-  },
-  receiveMsg: function(msg) {
-    this.setMsg(msg);
-    this.clearErrors();
-    this.emitChange();
-  },
-  registerRecieveCallback: function(actionType) {
-    return AppDispatcher.register(function(action) {
-      if (action.actionType == actionType)
-        this.receiveData(action.data);
-    }.bind(this))
-  },
-  registerMessageCallback: function(actionType) {
-    return AppDispatcher.register(function(action) {
-      if (action.actionType == actionType)
-        this.receiveMsg(action.data);
-    }.bind(this));
-  }
-});
+  });
 
   var store = Object.create(StorePrototype);
   if (registerAction) {
