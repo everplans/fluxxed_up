e7a6a1e833298a4e6e560ffd33b111af3c5e8fc9
Bart Riordan <bartriordan@gmail.com>
2015-11-21 14:57:35 -0500

    Remove extra space in import statement

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 export ActionPrototype from './lib/ActionPrototype'
-export  ExtraStorage from './lib/extra_storage'
+export ExtraStorage from './lib/extra_storage'
 export Dispatcher from './lib/ep-dispatcher'
 export JsonFetcher from './lib/jsonFetcher'
 //export FeatureFlags from './lib/FeatureFlags' -- haven;t quite figured out the singleton pattern here
