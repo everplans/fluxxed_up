3da8e155c9afcb5c2b4ce3e2c28cc1e606c4fbac
Bart Riordan <bartriordan@gmail.com>
2016-05-13 09:50:28 -0400

    Cleanup webpack config and kill dead code

--- webpack.config.js
+++ webpack.config.js
@@ -1,19 +1,14 @@
-var webpack = require('webpack');
-
-var dedup =
-  new webpack.optimize.DedupePlugin();
-
 module.exports = {
   entry: {
     app: ['./src/index.js']
   },
   module: {
     loaders: [
-      { test: /\.js$/, exclude: [/node_modules/], loader: 'babel-loader' }, //'react-hot',
-      { test: /\.scss$/, loader: "style-loader!css-loader!sass-loader"},
-      { test: /\.styl$/, loader: 'style-loader!css-loader!stylus-loader?paths=node_modules/'},
-      { test: /\.less$/, loader: "style-loader!css-loader!less-loader" },
-      { test: /\.(svg|png|gif)$/, loader: "url-loader?limit=8192" }
+      {test: /\.js$/, exclude: [/node_modules/], loader: 'babel-loader'},
+      {test: /\.scss$/, loader: 'style-loader!css-loader!sass-loader'},
+      {test: /\.styl$/, loader: 'style-loader!css-loader!stylus-loader?paths=node_modules/'},
+      {test: /\.less$/, loader: 'style-loader!css-loader!less-loader'},
+      {test: /\.(svg|png|gif)$/, loader: 'url-loader?limit=8192'}
     ]
   },
   jshint: {
@@ -23,11 +18,5 @@ module.exports = {
   },
   resolve: {
     extensions: ['', '.js']
-  },
-  plugins: [
-   // new AssetPlugin({path: __dirname +'/build'})
-
-    //new webpack.HotModuleReplacementPlugin(),
-    //new webpack.NoErrorsPlugin()
-  ]
+  }
 }
