80c94f813b2bdaa58d408aba25b5c88290bd8e14
Bart Riordan <bartriordan@gmail.com>
2016-05-13 15:11:52 -0400

    Update comments for clarity and grammar

--- src/lib/ActionFactory.js
+++ src/lib/ActionFactory.js
@@ -4,21 +4,19 @@ import ActionPrototype from './ActionPrototype'
 import AppDispatcher from './fu-dispatcher'
 
 export default class ActionFactory {
-  // takes an array of strings which become action Types, via KeyMirror
+  // Take an array of strings which are converted to action Types via keyMirror:
   constructor(actionTypes) {
     this.Types = keyMirror(actionTypes.reduce((previous, keyName) => {
       previous[keyName] = null
       return previous
     }, {}))
-    Object.assign(this, ActionPrototype) // eventually we'll make all actions use this class and we don't need the prototype
+    Object.assign(this, ActionPrototype) // TODO: make all actions use this class and won't need the prototype
   }
-
   buildAction(name, method, url, options) {
     this[name] = data => this.fireApi(method, url, data, options)
   }
-  // builds an action function that just dispatches a type with a passed data payload
+  // Build an action function that just dispatches a Type with the passed data payload:
   buildActionDispatch(name, type) {
     this[name] = data => AppDispatcher.dispatch({actionType: type, data})
   }
-
 }
