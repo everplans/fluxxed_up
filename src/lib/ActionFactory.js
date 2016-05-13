eb65902a88680b8c7ba1c3add2212395510172f8
Kayode Adeniyi <kayode.adeniyi@andela.co>
2016-05-13 17:56:08 +0100

    Add ActionFactory

---
+++ src/lib/ActionFactory.js
@@ -0,0 +1,35 @@
+import KeyMirror from 'keymirror'
+
+import ActionPrototype from './ActionPrototype'
+import AppDispatcher from './fu-dispatcher'
+import fetcher from './jsonFetcher'
+
+export default class ActionFactory {
+  //takes an array of 'strings which become action Types, via KeyMirror
+  constructor(actionTypes) {
+    this.Types = KeyMirror(actionTypes.reduce((previous, keyName) => {
+      previous[keyName] = null
+      return previous
+    }, {}))
+    Object.assign(this, ActionPrototype) //eventually we'll make all actions use this class and we don't need the prototype
+  }
+
+  /* -- builds the action method and calls fireApi for you example usage
+    myActions.buildAction(
+      'updateAssessment',
+      'put',
+      'quickplan/update',
+      { successAction: myActions.Types.GOT_ASSESSMENT_QUESTION}
+    )
+  */
+  buildAction(name, method, url, options) {
+    this[name] = function(data) {
+      this.fireApi(method, url, data, options)
+    }
+  }
+  /* -- builds an action function that just dispatches a type with a passed data payload */
+  buildActionDispatch(name, type) {
+    this[name] = data => AppDispatcher.dispatch({data: data, actionType: type})
+  }
+
+}
