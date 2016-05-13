f0b3662699bd18fb12dfa26f69eab1b5bb45f6f5
Adepoju Adebayo <mradeybee@gmail.com>
2016-05-13 21:01:25 +0100

    replace actionPrototype with that of webapps

--- src/lib/ActionPrototype.js
+++ src/lib/ActionPrototype.js
@@ -1,63 +1,64 @@
-import fetcher from '../lib/jsonFetcher'
-import AppDispatcher from '../lib/fu-dispatcher'
-import KeyMirror from 'keymirror'
-
-// TODOS:
-// --turn into a class.
-// --make it a factory function like we do with store prototype.
+var fetcher = require('../lib/jsonFetcher');
+var AppDispatcher = require('../lib/ep-dispatcher');
+var KeyMirror = require('keymirror');
 
 var ActionPrototype = {
   specialTypes: KeyMirror({
     GOT_MSG: null
   }),
   originalActions: {},
-  stubForTests() {
+  stubForTests: function() {
     this.receivedData = {}
-    const actions = Object.getOwnPropertyNames(this).filter(fn => typeof this[fn] === 'function')
-    actions.forEach(action => {
+    let actions = Object.getOwnPropertyNames(this).filter( (fn)=> {return typeof this[fn] === 'function' } )
+    actions.forEach( (action)=> {
       if (['fireApi', 'stubForTests', 'stubbedAction', 'saveAction', 'restore'].indexOf(action) < 0) {
         this.saveAction(action)
-        this[action] = data => { this.stubbedAction(action, data) }
+        this[action]=(data)=> {this.stubbedAction(action, data)}
       }
     })
   },
-  saveAction(action) { this.originalActions[action] = this[action] },
-  stubbedAction(actionName, data) { this.receivedData[actionName] = data },
-  restore() {
-    const originals = Object.getOwnPropertyNames(this.originalActions).filter(action => typeof this.originalActions[action] === 'function')
-    originals.forEach(action => { this[action] = this.originalActions[action] })
+  stubbedAction: function(actionName, data) {
+    this.receivedData[actionName] = data
+  },
+  saveAction: function(action) {
+    this.originalActions[action] = this[action]
+  },
+  restore: function() {
+    let orignals = Object.getOwnPropertyNames(this.originalActions).filter((fn)=>{ return typeof this.originalActions[fn] === 'function'})
+    orignals.forEach((fn)=>{this[fn] = this.originalActions[fn]})
     this.originalActions = {}
   },
-  fireApi(method, url, data, options) {
-    // set default values
+  fireApi: function(method, url, data, options) {
+    //set default values
     var errorAction = options.errorAction ? options.errorAction : options.successAction
-    fetcher[method](fetcher, url, data)
-      .then(successData => {
-        if (options.successAction) {
+    fetcher[method].call(fetcher,url, data)
+      .then(function(data) {
+        var scopedData = options.JSONHead ? data[options.JSONHead] : data
+        if (options['successAction']) {
           AppDispatcher.dispatch({
-            actionType: options.successAction,
-            data: (options.JSONHead ? successData[options.JSONHead] : successData)
+            actionType: options['successAction'],
+            data: scopedData
           })
         }
-        if (options.onSuccess)
-          options.onSuccess.apply()
-
+        if (options.onSuccess) {
+          options.onSuccess.apply(null, [scopedData])
+        }
         if (options.successMsg) {
           AppDispatcher.dispatch({
             actionType: this.specialTypes.GOT_MSG,
             data: options.successMsg
-          })
+          });
         }
       }.bind(this))
-      .fail(failureData => {
-        var scopedErrors = options.JSONHead ? failureData[options.JSONHead] : failureData
-        var errors = scopedErrors ? scopedErrors.errors : failureData.errors
+      .fail(function(data) {
+        var scopedErrors = options.JSONHead ? data[options.JSONHead] : data
+        var errors = scopedErrors ? scopedErrors.errors : data.errors
         AppDispatcher.dispatch({
           actionType: errorAction,
-          data: {errors}
+          data: {errors: errors}
         })
       })
   }
 }
 
-export default ActionPrototype
+module.exports = ActionPrototype;
