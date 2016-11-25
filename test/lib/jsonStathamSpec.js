eaf5411610bc0d95bd37bba3e54b1e251e825f57
Bart Riordan <bartriordan@gmail.com>
2016-11-25 14:39:41 -0500

    Clean up and organize tests a bit

--- test/lib/jsonStathamSpec.js
+++ test/lib/jsonStathamSpec.js
@@ -8,10 +8,11 @@ class TestAdaptor extends AjaxAdaptorBase {
   serverBase() { return 'http://test.com' }
 }
 
+let server
+let spy
+const getArgumentsPassedToSpy = () => spy.getCall(0).args[0]
+
 describe('jsonStatham', () => {
-  var server
-  var spy
-  const getArgumentsPassedToSpy = () => spy.getCall(0).args[0]
 
   before(() => jsonStatham.setAdaptor(new TestAdaptor()))
 
@@ -25,7 +26,7 @@ describe('jsonStatham', () => {
   })
 
   describe('mechanics: ', () => {
-    it('builds get request', () => {
+    it('builds GET request', () => {
       jsonStatham.get('/bla')
       expect(getArgumentsPassedToSpy().url).to.equal('http://test.com/api/bla')
     })
@@ -57,7 +58,7 @@ describe('jsonStatham', () => {
       expect(spyData.data.param).to.equal('some data')
     })
 
-    it('builds delete request', () => {
+    it('builds DELETE request', () => {
       jsonStatham.delete('/bla', {param: 'some data'})
       const spyData = getArgumentsPassedToSpy()
       expect(spyData.url).to.equal('http://test.com/api/bla')
@@ -99,49 +100,51 @@ describe('jsonStatham', () => {
       })
     })
 
-    it('POST data', done => {
-      utils.createServerAndMock('POST', '/api/test', JSON.stringify({fun: 'times for post'}), server, 200)
-
-      jsonStatham.post('/test').done(data => {
-        expect(data.fun).to.equal('times for post')
-        done()
-      })
-    })
-
-    it('POST data with error', done => {
-      utils.createServerAndMock('POST', '/api/test', JSON.stringify({error: {message: 'oops'}}), server, 422)
-      jsonStatham.post('/test').done(function() {}).fail(data => {
-        expect(data.error.message).to.equal('oops')
-        done()
-      })
-    })
-
-    it('POST file', done => {
-      utils.createServerAndMock('POST', '/api/test', JSON.stringify({fun: 'times for post'}), server, 200)
-      var formData = new FormData()
-      formData.append('comments', 'hey there', 'stuff')
-      jsonStatham.postFile('/test').done(data => {
-        expect(data.fun).to.equal('times for post')
-        done()
-      })
-    })
-
-    it('POST file with error', done => {
-      utils.createServerAndMock('POST', '/api/test', JSON.stringify({error: {message: 'oops'}}), server, 422)
-      var formData = new FormData()
-      formData.append('comments', 'hey there', 'stuff')
-      jsonStatham.postFile('/test', formData).fail(data => {
-        expect(data.error.message).to.equal('oops')
-        done()
-      })
-    })
-
     it('does not attempt to parse whitespace-only strings as JSON', done => {
       const whitespaceOnly = '   '
       utils.createServerAndMock('GET', '/api/test', whitespaceOnly, server, 200)
       jsonStatham.get('/test').done(data => {
         expect(data).to.equal(whitespaceOnly)
         done()
+      })
+    })
+
+    describe('POST:', () => {
+      it('POST data', done => {
+        utils.createServerAndMock('POST', '/api/test', JSON.stringify({fun: 'times for post'}), server, 200)
+
+        jsonStatham.post('/test').done(data => {
+          expect(data.fun).to.equal('times for post')
+          done()
+        })
+      })
+
+      it('POST data with error', done => {
+        utils.createServerAndMock('POST', '/api/test', JSON.stringify({error: {message: 'oops'}}), server, 422)
+        jsonStatham.post('/test').done(function() {}).fail(data => {
+          expect(data.error.message).to.equal('oops')
+          done()
+        })
+      })
+
+      it('POST file', done => {
+        utils.createServerAndMock('POST', '/api/test', JSON.stringify({fun: 'times for post'}), server, 200)
+        var formData = new FormData()
+        formData.append('comments', 'hey there', 'stuff')
+        jsonStatham.postFile('/test').done(data => {
+          expect(data.fun).to.equal('times for post')
+          done()
+        })
+      })
+
+      it('POST file with error', done => {
+        utils.createServerAndMock('POST', '/api/test', JSON.stringify({error: {message: 'oops'}}), server, 422)
+        var formData = new FormData()
+        formData.append('comments', 'hey there', 'stuff')
+        jsonStatham.postFile('/test', formData).fail(data => {
+          expect(data.error.message).to.equal('oops')
+          done()
+        })
       })
     })
   })
