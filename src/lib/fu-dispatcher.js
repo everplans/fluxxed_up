0997cdbeb3b88f7b07f8a0e01fac9018ca5854ea
Bart Riordan <bartriordan@gmail.com>
2016-12-23 12:58:57 -0500

    Clean up a few bits of lint

--- src/lib/fu-dispatcher.js
+++ src/lib/fu-dispatcher.js
@@ -3,7 +3,7 @@ import assign from 'object-assign'
 import invariant from 'invariant'
 
 var AppDispatcher = assign(new Dispatcher(), {
-  dispatch: function(action) {
+  dispatch(action) {
     invariant(action.actionType, 'action type is undefined.')
     Dispatcher.prototype.dispatch.call(this, action)
   }
