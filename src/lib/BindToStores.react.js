b81e7d24054523266e31b3bcb5aece5eef439b73
Bart Riordan <bartriordan@gmail.com>
2016-12-27 11:07:14 -0500

    Use let not var or const

--- src/lib/BindToStores.react.js
+++ src/lib/BindToStores.react.js
@@ -9,26 +9,25 @@ export default function bindResources(Component, resources, onBoundUpdate = null
   const stores = {}
   function storeNames() { return Object.getOwnPropertyNames(stores) }
 
-  var resourceConfigs = (Array.isArray(resources) ? resources : [resources])  // Handle old, one-resource pattern too.
+  let resourceConfigs = (Array.isArray(resources) ? resources : [resources])  // Handle old, one-resource pattern too.
 
-  var bootObjects = []  // Objects containing all necessary information for firing the bootAction of each resource
+  let bootObjects = []  // Objects containing all necessary information for firing the bootAction of each resource
     // and the tracking of the loading state of each resource.
     // Example of the bootObject for an 'item' resource:
        // {bootAction: 'fetchClient', actionClass: 'ClientActions', resourceType: 'item', resourceId: '12'}
 
   // Construct a bootObject for every resource:
   resourceConfigs.forEach(resourceConfig => {
-    const resourceType = Object.getOwnPropertyNames(resourceConfig)[0]
-    const resourceName = resourceConfig[resourceType]
+    let resourceType = Object.getOwnPropertyNames(resourceConfig)[0]
+    let resourceName = resourceConfig[resourceType]
 
-    // Get the action class--previously registered with the Container--and action Type used to fetch the resource data:
-    const actionClass = Container.getAction(resourceName)
-    const actionType = actionClass.Types[`GOT_${resourceName.toUpperCase()}`]
+    // Get the action class--previously registered with the Container--used to fetch the resource data:
+    let actionClass = Container.getAction(resourceName)
 
     // Get or create the store used to hold the resource data:
-    var resourceStore = Container.getStore(resourceName)
+    let resourceStore = Container.getStore(resourceName)
     if (!resourceStore)
-      resourceStore = Container.registerStore(resourceName, storePrototype(actionType))
+      resourceStore = Container.registerStore(resourceName, storePrototype(actionClass.Types[`GOT_${resourceName.toUpperCase()}`]))
 
     // Add the store to the list of store the BoundComponent will listen to:
     stores[resourceName] = resourceStore
@@ -53,17 +52,23 @@ export default function bindResources(Component, resources, onBoundUpdate = null
     componentWillMount() {
       // Create a listener for each resource store:
       storeNames().forEach(store => {
-        const onChangeHandlerName = `handle${store}Change`
+        let onChangeHandlerName = `handle${store}Change`
         this[onChangeHandlerName] = this.onChangeFactory(store)
         stores[store].addChangeListener(this[onChangeHandlerName])
       })
 
       // Fire the bootAction to fetch each resource:
-      var loadingStates = {}
+      let loadingStates = {}
+
       bootObjects.forEach(bootObject => {
         loadingStates[bootObject.loadingName] = true
 
-        /* eslint-disable indent */
+        // Need to hoist these values when called again from componentWillReceiveProps (BJK):
+        let resourceId = bootObject.resourceId
+        let loadingName = bootObject.loadingName
+        let bootAction = bootObject.bootAction
+
+        /* eslint-disable indent */ // Fix ESLint complaining about our preferred switch syntax.
         // Choose which ID--if any--is passed to the GET request to identify the resource:
         switch (bootObject.resourceType) {
           case 'item':  // One specific item that MUST have an ID included in the GET request.
@@ -97,10 +102,10 @@ export default function bindResources(Component, resources, onBoundUpdate = null
     },
 
     onChangeFactory(store) {  // Create a custom listener identifier so loading states can be tracked independently for each resource.
-      var myStore = store
+      let myStore = store
       return () => {
-        var newState = {loading: false}  // Backwards-compatability for single-resource bindings.
-        var newStoreState = stores[myStore].getState()
+        let newState = {loading: false}  // Backwards-compatability for single-resource bindings.
+        let newStoreState = stores[myStore].getState()
         newState[myStore] = newStoreState
         newState[`loading${capitalize(store)}`] = false
 
@@ -114,5 +119,6 @@ export default function bindResources(Component, resources, onBoundUpdate = null
 
     render() { return <Component {...this.props} {...this.state} /> }
   })
+
   return BoundComponent
 }
