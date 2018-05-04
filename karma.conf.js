c678f5070a0ba24fffdafc11321e0bf8e137cd58
Bart Riordan <bartriordan@gmail.com>
2018-05-04 16:10:16 -0400

    Update some dependencies and add PhantomJS shim

--- karma.conf.js
+++ karma.conf.js
@@ -8,6 +8,7 @@ module.exports = function(config) {
     webpack: webpackConfig,
     frameworks: ['es5-shim', 'mocha', 'chai', 'sinon-chai', 'chai-jquery', 'jquery-2.1.0'],
     files: [
+      './node_modules/es6-shim/es6-shim.js',
       './test/index.js'
     ],
     preprocessors: {
